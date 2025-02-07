8bd5e35948cd24f4481b94cb5d4aeb27a7d8ea1a
Will Harrison <willharrison2016@gmail.com>
2025-02-07 04:52:16 +0000

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}", // Ensure Tailwind scans all app files
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
