597eb425668040f75201bca3b047d2a4b592b234
Will Harrison <willharrison2016@gmail.com>
2025-02-07 20:41:02 -0600

    Update page.tsx

--- src/app/lesson-1/page.tsx
+++ src/app/lesson-1/page.tsx
@@ -1,3 +1,5 @@
+"use client";  // Add this line at the top
+
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import RefiningAnimation from "@/components/RefiningAnimation";
